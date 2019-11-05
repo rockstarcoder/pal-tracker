1846cb755cb8d33504fcbd3ebb9714f531c65931
pal_user <tom@training.com>
2019-11-05 09:41:29 -0700

    reverted welcome message

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -19,6 +19,6 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String sayHello() {
-        return welcomeMessage + " with new text";
+        return welcomeMessage;
     }
 }
